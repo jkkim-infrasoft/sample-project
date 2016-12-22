51e5896b59f7c2b58659df1d61dcc6fbb7a82db5
김정기 <jkkim@infrasoft.co.kr>
2016-12-22 12:37:32 +0900

    Tidied AppInitializer

--- src/main/java/kr/co/infrasoft/sample_project/config/AppInitializer.java
+++ src/main/java/kr/co/infrasoft/sample_project/config/AppInitializer.java
@@ -1,64 +1,35 @@
 package kr.co.infrasoft.sample_project.config;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class AppInitializer implements WebApplicationInitializer {
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		/*
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.register(AppConfig.class);
-		ctx.setServletContext(container);
 
-		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+    @Override
+    public void onStartup(ServletContext container) {
+      // Create the 'root' Spring application context
+      AnnotationConfigWebApplicationContext rootContext =
+        new AnnotationConfigWebApplicationContext();
+      rootContext.register(AppConfig.class);
 
-		servlet.setLoadOnStartup(1);
-		servlet.addMapping("/");
-		
-        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-		
-		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-		
-		container.addFilter("characterEncoding", characterEncodingFilter).addMappingForUrlPatterns(dispatcherTypes, true, "/");
-		*/
-		//If you want to use the XML configuration, comment the following two lines out.
-        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(AppConfig.class);
-        appContext.setDisplayName("removed customer name");       
+      // Manage the lifecycle of the root application context
+      container.addListener(new ContextLoaderListener(rootContext));
 
-        //If you want to use the XML configuration, uncomment the following lines.
-        //XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
-        //rootContext.setConfigLocation("classpath:mvc-servlet.xml");
+      // Create the dispatcher servlet's Spring application context
+      AnnotationConfigWebApplicationContext dispatcherContext =
+        new AnnotationConfigWebApplicationContext();
+      //dispatcherContext.register(DispatcherConfig.class);
 
-        AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
-        //mvcContext.register(ServletConfig.class);
-
-        ServletRegistration.Dynamic springmvc =
-                servletContext.addServlet("dispatcher",
-                          new DispatcherServlet(mvcContext));
-        springmvc.setLoadOnStartup(1);
-        springmvc.addMapping("/");
-
-        //EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        servletContext.addFilter("characterEncoding", characterEncodingFilter).addMappingForUrlPatterns(null, true, "/*");
-
-        /*FilterRegistration.Dynamic security = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
-        security.addMappingForUrlPatterns(dispatcherTypes, true, "/*");*/
-
-        servletContext.addListener(new ContextLoaderListener(appContext));
-	}
+      // Register and map the dispatcher servlet
+      ServletRegistration.Dynamic dispatcher =
+        container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+      dispatcher.setLoadOnStartup(1);
+      dispatcher.addMapping("/");
+    }
 
 }
